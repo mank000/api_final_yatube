5054b9e493fbb50a1dfcb0331b728301a953e335
Козьмин Артём <kozmin_arti@mail.ru>
2024-04-28 18:06:51 +0700

    first-commt

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,10 +5,8 @@ class OwnerOrReadOnly(permissions.BasePermission):
     """Permission для создателей объекта."""
 
     def has_permission(self, request, view):
-        return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         if request.method not in permissions.SAFE_METHODS:
